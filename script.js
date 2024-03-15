7fc0ae8bff634ed035f605435343ede528c7149c
”MarcoRomanoHub” <marcoromano270@gmail.com>
2024-03-15 12:16:26 +0100

    [add] Milestone 3 complete

--- script.js
+++ script.js
@@ -24,11 +24,22 @@ createApp({
       }
       this.contacts[this.activeContactId].messages.push(newMessage),
       this.newMessageItem = ''
+
+      const newMessageAnswer = {
+        date: '10/01/2020 15:30:55',
+        message: 'Ok',
+        status: 'received'
+      }
+      setTimeout( ()=> {
+        this.contacts[this.activeContactId].messages.push(newMessageAnswer)
+        
+      },1000 )
+
     }
   },
 
   mounted(){
-    activeContactId = activeContactId
+
   },
 
   created(){
